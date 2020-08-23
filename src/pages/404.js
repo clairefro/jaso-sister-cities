db742033fe316fb1349a66e12dad6a17fcb7870a
claire froelich <claire.froelich@gmail.com>
2020-08-23 09:01:09 -0700

    add 404

---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const NotFound = () => (
+  <div>
+    <h1>Page not found</h1>
+    <h1>ページが見つかりませんでした</h1>
+    <p>
+      Find the JASO Sister Cities Map <Link to="/">here</Link>
+    </p>
+    <p>
+      オレゴン日米協会姉妹都市マップは<Link to="/">こちら</Link>
+    </p>
+  </div>
+)
+
+export default NotFound
